1ce49c9b76a8ce9928117b74ddd3712f3bb180e7
MatiasUC <mtoro13@hotmail.com>
2013-09-09 22:45:56 -0300

    Commit Inicial
    Primer commit de la aplicación a GitHub

---
+++ MvcReservasWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,59 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MvcReservasWeb
+{
+    public class BundleConfig
+    {
+        // Para obtener más información acerca de Bundling, consulte http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de creación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            //Bundles de Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                            "~/Scripts/bootstrap.min.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/placeholder").Include(
+                "~/Scripts/jquery.placeholder.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap/css").Include(
+                            //"~/Content/bootstrap/bootstrap-theme.min.css",
+                            "~/Content/bootstrap/bootstrap.css"));
+            //Fin Bundles de Bootstrap
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/RWeb.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+            
+        }
+    }
+}
